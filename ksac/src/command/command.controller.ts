4ac537ce218156bf60e6ce887ce32ed83c294cc1
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2024-07-15 20:05:50 -0300

    ksac: Command mocks

--- ksac/src/command/command.controller.ts
+++ ksac/src/command/command.controller.ts
@@ -1,7 +1,34 @@
-import { injectable } from "inversify";
-import { Controller } from "src/controller";
+import { Command } from 'commander';
+import { injectable } from 'inversify';
+import { Controller } from 'src/controller';
+import { CommandService } from './command.service';
 
 @injectable()
 export class CommandController implements Controller {
-    async registerCommands() { }
+    constructor(
+        private readonly command: Command,
+        private readonly service: CommandService,
+    ) { }
+
+    async registerCommands() {
+        this.command
+            .command('validate')
+            .description('Checks the KSaC definitions for errors or warnings')
+            .action(() => this.service.validate());
+
+        this.command
+            .command('plan')
+            .description('Shows the changes that will be made by the KSaC definitions')
+            .action(() => this.service.plan());
+
+        this.command
+            .command('apply')
+            .description('Apply changes to the StackSpot resources so they match the KSaC definitions')
+            .action(() => this.service.apply());
+
+        this.command
+            .command('destroy')
+            .description('Destroys the resources defined in the KSaC definitions')
+            .action(() => this.service.destroy());
+    }
 }
---
+++ ksac/src/command/command.service.ts
@@ -0,0 +1,32 @@
+import { injectable } from 'inversify';
+import 'colors';
+
+const ADD = '+'.green.bold;
+const DELETE = '-'.red.bold;
+const MODIFY = '~'.yellow.bold;
+const KEEP = '@'.gray.bold;
+
+@injectable()
+export class CommandService {
+    async validate() {
+        console.log('The definitions are valid');
+    }
+
+    async plan() {
+        console.log('The following changes will be made:');
+        console.log(`  ${ADD} Create Knowledge Source vivo-easy-schema`);
+        console.log(`    ${ADD} Create Knowledge Object client-table`);
+        console.log(`    ${ADD} Create Knowledge Object payment-table`);
+        console.log(`    ${ADD} Create Knowledge Object transaction-table`);
+        console.log(`  ${MODIFY} Rename Knowledge Source operation-alerts`);
+        console.log(`  ${KEEP} On Knowledge Source onboarding`);
+        console.log(`    ${DELETE} Delete Knowledge Object 01J2M2NF01AKVBPT4NA35CPTHD[a18b4a0b]`);
+        console.log(`    ${ADD} Create Knowledge Object pandora`);
+    }
+
+    async apply() {
+    }
+
+    async destroy() {
+    }
+}
