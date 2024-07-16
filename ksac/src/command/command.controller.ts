585b442598ee4ac3ddce5b70d2b08780f70d0b88
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2024-07-16 08:33:00 -0300

    ksac: Added not implemented errors

--- ksac/src/command/command.controller.ts
+++ ksac/src/command/command.controller.ts
@@ -2,6 +2,7 @@ import { Command } from 'commander';
 import { injectable } from 'inversify';
 import { Controller } from 'src/controller';
 import { CommandService } from './command.service';
+import { CommandError } from './command.error';
 
 @injectable()
 export class CommandController implements Controller {
@@ -14,21 +15,34 @@ export class CommandController implements Controller {
         this.command
             .command('validate')
             .description('Checks the KSaC definitions for errors or warnings')
-            .action(() => this.service.validate());
+            .action(() => this.errorProxy(() => this.service.validate()));
 
         this.command
             .command('plan')
             .description('Shows the changes that will be made by the KSaC definitions')
-            .action(() => this.service.plan());
+            .action(() => this.errorProxy(() => this.service.plan()));
 
         this.command
             .command('apply')
             .description('Apply changes to the StackSpot resources so they match the KSaC definitions')
-            .action(() => this.service.apply());
+            .action(() => this.errorProxy(() => this.service.apply()));
 
         this.command
             .command('destroy')
             .description('Destroys the resources defined in the KSaC definitions')
-            .action(() => this.service.destroy());
+            .action(() => this.errorProxy(() => this.service.destroy()));
+    }
+
+    private async errorProxy<R, F extends (...args: any[]) => Promise<R>>(fn: F): Promise<R> {
+        try {
+            return await fn();
+        } catch(e) {
+            if (e instanceof CommandError) {
+                console.error(e.message.red);
+                process.exit(1);
+            }
+
+            throw e;
+        }
     }
 }
--- ksac/src/command/command.service.ts
+++ ksac/src/command/command.service.ts
@@ -1,6 +1,7 @@
 import { injectable } from 'inversify';
 import 'colors';
 import { DefinitionServices } from '../definition/definition.service';
+import { CommandError } from './command.error';
 
 const ADD = '+'.green.bold;
 const DELETE = '-'.red.bold;
@@ -19,20 +20,14 @@ export class CommandService {
     }
 
     async plan() {
-        console.log('The following changes will be made:');
-        console.log(`  ${ADD} Create Knowledge Source vivo-easy-schema`);
-        console.log(`    ${ADD} Create Knowledge Object client-table`);
-        console.log(`    ${ADD} Create Knowledge Object payment-table`);
-        console.log(`    ${ADD} Create Knowledge Object transaction-table`);
-        console.log(`  ${MODIFY} Rename Knowledge Source operation-alerts`);
-        console.log(`  ${KEEP} On Knowledge Source onboarding`);
-        console.log(`    ${DELETE} Delete Knowledge Object 01J2M2NF01AKVBPT4NA35CPTHD[a18b4a0b]`);
-        console.log(`    ${ADD} Create Knowledge Object pandora`);
+        throw new CommandError('Not implemented yet');
     }
 
     async apply() {
+        throw new CommandError('Not implemented yet');
     }
 
     async destroy() {
+        throw new CommandError('Not implemented yet');
     }
 }
